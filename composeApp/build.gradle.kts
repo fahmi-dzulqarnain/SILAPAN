1e4d841780a9ad0a37542cdb6150709123eff4bf
Fahmi Dzulqarnain <fahmi.dzulqarnain@silentmode.my>
2025-12-15 21:06:04 +0700

    Bump app version to 1.4 (versionCode 3)
    Updated the Android app's versionCode to 3 and versionName to 1.4 in build.gradle.kts to reflect a new release.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -89,8 +89,8 @@ android {
         applicationId = "com.midcores.silapan"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.4"
     }
     packaging {
         resources {
